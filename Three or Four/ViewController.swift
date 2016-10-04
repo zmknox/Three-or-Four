f21ad58d95cefeed4bdc26ff6926ee034f8a751c
Zach Knox <zmknox@gmail.com>
2016-10-03 20:03:55 -0400

    Does what it says on the tin

--- Three or Four/ViewController.swift
+++ Three or Four/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet var rollResult: UILabel!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,11 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func rollButton(_ sender: AnyObject) {
+		let r = arc4random_uniform(2) + 3
+		rollResult.text = "\(r)"
+		
+	}
 
 }
 
